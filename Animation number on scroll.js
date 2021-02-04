033bd990436d35fa8fe1d3bf5eb8e9c46e55a98c
hoby-qode <54716724+hoby-qode@users.noreply.github.com>
2021-02-04 14:14:49 +0300

    Animate number on scroll
    Animate number on scroll

---
+++ Animation number on scroll.js
@@ -0,0 +1,29 @@
+$(window).scroll(myscroll);
+  var viewed = false;
+
+  function isScrolledIntoView(elem) {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(elem).height();
+
+      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
+  function myscroll() {
+    if (isScrolledIntoView($(".bloc-stats")) && !viewed) {
+        viewed = true;
+        $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+      });
+    }
+  }
